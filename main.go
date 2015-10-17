2e17c2797dcbadc151cdbe5a666d9c42fcfa6147
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-10-17 14:42:30 +0200

    Add LoadFile with the most basic structure

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Info is the main storage for information. All yaml files map to this.
+type Info struct {
+	ID   string
+	Type string `yaml:"type"`
+}
+
+// LoadInfo loads an Info object from a file path
+func LoadInfo(p string) (i Info, err error) {
+	data, err := ioutil.ReadFile(p)
+	if err != nil {
+		log.Fatal("Reading file failed: ", p)
+	}
+
+	basename := filepath.Base(p)
+	id := strings.TrimSuffix(basename, filepath.Ext(basename))
+
+	i = Info{ID: id}
+	yaml.Unmarshal(data, &i)
+	return
+}
+
+func main() {
+	i, err := LoadInfo("test/data/first.yml")
+	if err != nil {
+		log.Fatal("error pls")
+	}
+
+	fmt.Println(i)
+}
