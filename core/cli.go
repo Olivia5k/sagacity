a2ce1864c15288da4e6001a8a9cd0f217d1a5a7c
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-10-17 18:09:10 +0200

    Add CLI, refactor main.go

---
+++ core/cli.go
@@ -0,0 +1,19 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/codegangsta/cli"
+)
+
+// BuildCLI builds the base CLI App() object
+func BuildCLI() (app *cli.App) {
+	app = cli.NewApp()
+	app.Name = "sagacity"
+	app.Usage = "spread and use knowledge!"
+	app.Action = func(c *cli.Context) {
+		fmt.Println("hehe")
+	}
+
+	return
+}
--- sagacity/main.go
+++ sagacity/main.go
@@ -2,37 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"path/filepath"
-	"strings"
+	"os"
 
 	"github.com/thiderman/sagacity/core"
-	"gopkg.in/yaml.v2"
 )
-
-// Info is the main storage for information. All yaml files map to this.
-type Info struct {
-	ID   string
-	Type string `yaml:"type"`
-}
-
-// LoadInfo loads an Info object from a file path
-func LoadInfo(p string) (i Info, err error) {
-	data, err := ioutil.ReadFile(p)
-	if err != nil {
-		log.Fatal("Reading file failed: ", p)
-	}
-
-	basename := filepath.Base(p)
-	id := strings.TrimSuffix(basename, filepath.Ext(basename))
-
-	i = Info{ID: id}
-	yaml.Unmarshal(data, &i)
-	return
-}
 
 func main() {
 	conf := core.LoadConfig()
 	fmt.Println(conf)
+
+	app := core.BuildCLI()
+	app.Run(os.Args)
 }
