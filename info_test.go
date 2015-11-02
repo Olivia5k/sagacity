3080605cd08fab815ca36ad41f3388849b6a775c
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-11-02 11:16:17 +0100

    Add more unit tests to NewRepo()

--- info_test.go
+++ info_test.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+// createJunk creates a lot of garbage files in a temporary diretory
+func createJunk(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, os.FileMode(0700))
+	}
+
+	files, _ := ioutil.ReadDir(dir)
+	for i := len(files); i < 100; i++ {
+		// Since we are not using strconv, the strings created will almost
+		// literally be junk characters. Rather than numbers, they will be
+		// characters and special characters.
+		path := filepath.Join(dir, string(i))
+		os.Create(path)
+	}
+}
 
 func TestLoadInfo(t *testing.T) {
 	assert := assert.New(t)
@@ -40,4 +59,15 @@ func TestNewRepoLoadsMultipleFiles(t *testing.T) {
 	assert.Equal(r.Info["first"].ID, "first")
 	assert.Equal(r.Info["second"].ID, "second")
 	assert.Equal(len(r.Info), 2)
+}
+
+// Generate tons of junk files, and check that none of them are loaded
+func TestNewRepoIgnoresNonYamlJunk(t *testing.T) {
+	assert := assert.New(t)
+	dir := "test/junk/"
+	createJunk(dir)
+	r := NewRepo(dir)
+
+	assert.Equal(len(r.Info), 0)
+	assert.Equal(len(r.Control), 0)
 }
